a1dce69810e115ea20a21339088efcc6e27b2eba
Kerry Cheng <kerryyc@uci.edu>
2018-03-14 14:28:02 -0700

    Revert "Huu's recommandation other approach"

--- app/src/main/java/com/clarifai/android/starter/api/v2/Food.java
+++ app/src/main/java/com/clarifai/android/starter/api/v2/Food.java
@@ -1,7 +1,5 @@
 package com.clarifai.android.starter.api.v2;
 
-import java.util.Comparator;
-
 public class Food {
     //class that holds variables received from requests to Nutritionix and Yelp
 
@@ -15,5 +13,4 @@ public class Food {
     public String toString() {
         return name + ": " + calories;
     }
-
 }
